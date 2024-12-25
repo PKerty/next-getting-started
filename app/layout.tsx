b271762061db90483d05e47cbcd3efdef6fa04d5
kerty dev <pkerty.dev@gmail.com>
2024-12-25 01:05:29 -0300

    init push

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import "@/app/ui/global.css";
+import { inter, roboto } from "@/app/ui/fonts";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
